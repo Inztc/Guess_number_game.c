c230d20bc5763f2406219850f70c58a4d115f31c
Inztc <77892617+Inztc@users.noreply.github.com>
2022-02-19 20:36:23 +0700

    guessnum_game

---
+++ guessnumber.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{ 
+	int	guess;
+	int	i;
+	int	count = 5;
+	
+	printf("\nThis is a guessing game");
+	printf("\nI have chosen a number between 0 and 20 which you must guess\n");
+	
+	int	tries = 5;
+
+	while (tries > 0 && guess != 10) 
+	{
+		for (i = 1; i <= count; ++i)
+		{
+			printf("\nYou have %d tries left", tries);
+			printf("\nEnter a Guess: ");
+			scanf("%d", &guess);
+
+			if (guess > 10)
+			{	
+				printf ("\nSorry, %d is wrong, my number is less than that", guess);
+					 
+			}
+			else if (guess < 10)
+			{
+				printf( "\nSorry, %d is wrong, my number is more than that", guess);
+				
+			}
+			else 
+			{
+				//guess = 10;
+				printf("Congratulations. You guesses it right!\n");
+				break;
+			}
+			--tries;
+		}
+		
+	}
+	//printf("\n\nYou are running out of trial. You lose!\n");
+}
